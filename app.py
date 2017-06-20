05f5c600d2a0125cd8fef8f4ebee3b996ac4a83d
alishobeiri <alishobeiri@ymail.com>
2017-06-20 12:07:31 -0400

    RemovRemoved error causing paths and added folder for data

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import os
 server = Flask('my app')
 server.secret_key = os.environ.get('secret_key', 'secret')
 
-app = dash.Dash('UberApp', server=server, url_base_pathname='/dash/gallery/uber-rides/', csrf_protect=False)
+app = dash.Dash('UberApp', server=server) # , url_base_pathname='/dash/gallery/uber-rides/', csrf_protect=False)
 
 if 'DYNO' in os.environ:
     app.scripts.append_script({
@@ -26,7 +26,6 @@ mapbox_access_token = 'pk.eyJ1IjoiYWxpc2hvYmVpcmkiLCJhIjoiY2ozYnM3YTUxMDAxeDMzcG
 
 def initialize():
     df = pd.read_csv('output.csv')
-    df.drop("Unnamed: 0", 1, inplace=True)
     df["Date/Time"] = pd.to_datetime(df["Date/Time"], format="%Y-%m-%d %H:%M:%S")
     df.index = df["Date/Time"]
     df.drop("Date/Time", 1, inplace=True)
